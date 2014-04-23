6def575c4401b5e75a9a7f724fa48badbe8b1133
Brad Anderson <anderson.ab@gmail.com>
2014-04-23 11:51:49 -0700

    testing is working now. had a problem with not having JAVA_HOME set locally

--- src/main/java/com/andersab/camel/EnvironmentLoader.java
+++ src/main/java/com/andersab/camel/EnvironmentLoader.java
@@ -1,5 +1,9 @@
 package com.andersab.camel;
 
+import org.apache.camel.CamelContext;
+import org.apache.camel.model.ModelCamelContext;
+import org.apache.camel.model.RoutesDefinition;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -13,6 +17,10 @@ public class EnvironmentLoader {
      * The system property that the runtime properties are injected from
      */
     public static final String LOADER_SYSTEM_PROPERTY_NAME = "loaderProperties";
+    /**
+     * Directory in classpath to load routes from
+     */
+    public static final String ROUTES_XML_PROP_NAME = "routesXml";
 
     /**
      * Private constructor to protect utility class
@@ -26,12 +34,25 @@ public class EnvironmentLoader {
      * @see EnvironmentLoader#LOADER_SYSTEM_PROPERTY_NAME
      */
     public static void loadProperties() throws IOException {
-        Properties runtimeProps = new Properties();
-        String runtimePropertyLocation = System.getProperty(LOADER_SYSTEM_PROPERTY_NAME);
-        InputStream propertyFileResourceStream = EnvironmentLoader.class.getClassLoader().getResourceAsStream(runtimePropertyLocation);
+        Properties runtimeProps = System.getProperties();
+        InputStream propertyFileResourceStream = getInputStreamFromClasspath(LOADER_SYSTEM_PROPERTY_NAME);
         runtimeProps.load(propertyFileResourceStream);
         System.setProperties(runtimeProps);
     }
 
+    /**
+     * Loads the routes from the xml in the classpath
+     * @param camelContext
+     */
+    public static void loadRoutes(ModelCamelContext camelContext) throws Exception {
+        String routesDir = System.getProperty(ROUTES_XML_PROP_NAME);
+        InputStream routesInputStream = getInputStreamFromClasspath(ROUTES_XML_PROP_NAME);
+        RoutesDefinition routes = camelContext.loadRoutesDefinition(routesInputStream);
+        camelContext.addRouteDefinitions(routes.getRoutes());
+    }
 
+    private static InputStream getInputStreamFromClasspath(String artifactName) {
+        String runtimePropertyLocation = System.getProperty(artifactName);
+        return EnvironmentLoader.class.getClassLoader().getResourceAsStream(runtimePropertyLocation);
+    }
 }
--- src/test/java/com/andersab/camel/EnvironmentLoaderTest.java
+++ src/test/java/com/andersab/camel/EnvironmentLoaderTest.java
@@ -1,11 +1,21 @@
 package com.andersab.camel;
 
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.model.ModelCamelContext;
+import org.apache.camel.model.RouteDefinition;
+import org.apache.camel.model.RoutesDefinition;
 import org.junit.Test;
+
+import java.io.InputStream;
+import java.util.Collection;
 
 import static junit.framework.Assert.assertNotNull;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class EnvironmentLoaderTest {
+
+    public static final String ROUTE_XML = "routes/myRoutes.xml";
 
     /**
      * The following will load configuration properties from the classpath
@@ -15,7 +25,7 @@ public class EnvironmentLoaderTest {
     @Test
     public void testLoadingPropertiesFromClasspath() throws Exception {
         final String configurationPropertyFile = "config/loader.properties";
-        final String propertyInFile = "routesDir";
+        final String propertyInFile = EnvironmentLoader.ROUTES_XML_PROP_NAME;
 
         System.setProperty(EnvironmentLoader.LOADER_SYSTEM_PROPERTY_NAME, configurationPropertyFile);
         EnvironmentLoader.loadProperties();
@@ -23,6 +33,44 @@ public class EnvironmentLoaderTest {
         assertNotNull(result);
 
         String message = String.format("Assertion failure, check file ( %s ), looking for key ( %s )", configurationPropertyFile, propertyInFile);
-        assertEquals(message,"routes", result);
+        assertEquals(message, ROUTE_XML, result);
+    }
+
+    /**
+     * Test loading the routes in the path to the CamelContext
+     */
+    @Test
+    public void testLoadingRoutesFromClasspathToCamelContext() throws Exception {
+        final String routesXml = ROUTE_XML;
+        System.setProperty(EnvironmentLoader.ROUTES_XML_PROP_NAME, routesXml);
+        FakeCamelContext fakeCamelContext = new FakeCamelContext();
+        EnvironmentLoader.loadRoutes(fakeCamelContext);
+        assertTrue(fakeCamelContext.isLoadRoutesCalled());
+        assertTrue(fakeCamelContext.isAddRoutesCalled());
+    }
+
+    private class FakeCamelContext extends DefaultCamelContext {
+        private boolean loadRoutesCalled = false;
+        private boolean addRoutesCalled = false;
+
+        @Override
+        public synchronized RoutesDefinition loadRoutesDefinition(InputStream is) throws Exception {
+            this.loadRoutesCalled = true;
+            return super.loadRoutesDefinition(is);
+        }
+
+        @Override
+        public synchronized void addRouteDefinitions(Collection<RouteDefinition> routeDefinitions) throws Exception {
+            this.addRoutesCalled = true;
+            super.addRouteDefinitions(routeDefinitions);    //To change body of overridden methods use File | Settings | File Templates.
+        }
+
+        private boolean isLoadRoutesCalled() {
+            return loadRoutesCalled;
+        }
+
+        private boolean isAddRoutesCalled() {
+            return addRoutesCalled;
+        }
     }
 }
