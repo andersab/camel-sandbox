fd8767700227537198ad559ace96b8e31aa08d96
Brad Anderson <anderson.ab@gmail.com>
2014-04-25 09:41:05 -0700

    get rid of log4j debug logging

--- src/bin/run.sh
+++ src/bin/run.sh
@@ -5,7 +5,7 @@ APP_HOME=/tmp/camel-sandbox
 execJar=camel-sandbox-0.1.0-SNAPSHOT.jar
 LOG4J_PROPS="file:///$APP_HOME/config/log4j.properties"
 
-java -cp "$APP_HOME/lib/*" -DAPP_HOME="$APP_HOME" -Dlog4j.debug=true -Dlog4j.configuration="$LOG4J_PROPS" com.andersab.camel.Startup
+java -cp "$APP_HOME/lib/*" -DAPP_HOME="$APP_HOME" -Dlog4j.configuration="$LOG4J_PROPS" com.andersab.camel.Startup
 
 echo
 echo "Shutdown complete"
